412dff7672773df85fec2009406a59b256af8758
Kuba Szalaty <nnaisur@gmail.com>
2018-05-15 21:34:30 +0200

    Delete unused tests

--- src/app/directives/connect-form/connect-form.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ConnectFormDirective } from './connect-form.directive'
-
-describe('ConnectFormDirective', () => {
-    it('should create an instance', () => {
-        const directive = new ConnectFormDirective()
-        expect(directive).toBeTruthy()
-    })
-})
