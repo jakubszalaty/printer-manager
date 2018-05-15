fb43d0c62d161bc91fa47c06893cb236571ae833
Kuba Szalaty <nnaisur@gmail.com>
2018-05-15 23:08:08 +0200

    Add some e2e test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,28 @@
-import { AppPage } from './app.po';
+import { AppPage } from './app.po'
+import { browser, element, by } from 'protractor'
 
 describe('printer-manager App', () => {
-  let page: AppPage;
+    let page: AppPage
 
-  beforeEach(() => {
-    page = new AppPage();
-  });
+    beforeEach(() => {
+        page = new AppPage()
+    })
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
-  });
-});
+    it('should display app title', () => {
+        page.navigateTo()
+        expect(page.getToobarTitle()).toEqual('Printer Manager')
+    })
+
+    it('should return one element on list', () => {
+        page.navigateTo()
+        page.enterSearchText('HP')
+        expect(page.getPrintersList().count()).toEqual(1)
+    })
+
+    it('should add new printer to list', async () => {
+        page.navigateTo()
+        await page.waitForMainFab()
+        page.createNewPrinter()
+        expect(page.getLastAddedPrinterTitle()).toEqual('New printer (offline)')
+    })
+})
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,34 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element } from 'protractor'
 
 export class AppPage {
-  navigateTo() {
-    return browser.get('/');
-  }
+    navigateTo() {
+        return browser.get('/')
+    }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
-  }
+    getToobarTitle() {
+        return element(by.css('app-root mat-toolbar span')).getText()
+    }
+
+    enterSearchText(text: string) {
+        return element(by.css('app-root .search-form input')).sendKeys(text)
+    }
+
+    getPrintersList() {
+        return element.all(by.css('app-root .item-list mat-list-item'))
+    }
+
+    createNewPrinter() {
+        const button = element(by.css('app-root .main-fab button')).click()
+    }
+
+    waitForMainFab() {
+        return browser.wait(async () => {
+            const transform = await element(by.css('.main-fab')).getCssValue('transform')
+            return transform === 'matrix(1, 0, 0, 1, 0, 0)'
+        }, 3000)
+    }
+
+    getLastAddedPrinterTitle() {
+        return element(by.css('app-root .item-list mat-list-item:last-child h3')).getText()
+    }
 }
